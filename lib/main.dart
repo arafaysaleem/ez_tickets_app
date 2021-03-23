71ba1f247619b19967c169f92e3eed63c86410a9
arafaysaleem <a.rafaysaleem@gmail.com>
2021-03-23 06:20:11 -0700

    setup custom theme and router with material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:ez_ticketz_app/helper/utils/custom_theme.dart';
 import 'package:flutter/material.dart';
+
+import 'routes/router.gr.dart';
+import 'package:auto_route/auto_route.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final _appRouter = AppRouter();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
       debugShowCheckedModeBanner: false,
       title: 'EZ Tickets',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: CustomTheme.mainTheme,
     );
   }
 }
