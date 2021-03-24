a30c52d3e7da8f8d67714a8ec6ef7448926af62d
arafaysaleem <a.rafaysaleem@gmail.com>
2021-03-24 23:48:16 +0500

    home screen layout added

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            //Heading text
+            Text("EZ TICKETS", style: textTheme.headline1),
+
+            //Welcome msg
+            Text(
+              "Welcome to the new nueplex cinemas app",
+              style: textTheme.headline4,
+            ),
+
+            //experience msg
+
+            //Row
+              //login button
+              //face id button
+
+            //register button
+          ],
+        ),
+      ),
+    );
   }
 }
-
-
