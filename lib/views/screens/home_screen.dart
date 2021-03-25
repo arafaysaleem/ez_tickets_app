94fb23277cc602c5c10501173b98e977811124c8
arafaysaleem <a.rafaysaleem@gmail.com>
2021-03-26 01:35:00 +0500

    home screen added

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,31 +1,114 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_text_button.dart';
+
+import '../../helper/utils/constants.dart';
+import '../../helper/utils/assets_helper.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            //Heading text
-            Text("EZ TICKETS", style: textTheme.headline1),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 125, 20, 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Heading text
+              Text(
+                "EZ Tickets",
+                style: textTheme.headline1!.copyWith(
+                  color: theme.primaryColor,
+                ),
+              ),
 
-            //Welcome msg
-            Text(
-              "Welcome to the new nueplex cinemas app",
-              style: textTheme.headline4,
-            ),
+              const SizedBox(height: 35),
 
-            //experience msg
+              //Welcome msg
+              Text(
+                "Welcome to\nthe new\nNueplex cinemas",
+                style: textTheme.headline3!.copyWith(
+                  color: Colors.white,
+                ),
+              ),
 
-            //Row
-              //login button
-              //face id button
+              const SizedBox(height: 40),
 
-            //register button
-          ],
+              //experience msg
+              Text(
+                "New level of features\nwith the new app",
+                style: textTheme.headline5!.copyWith(
+                  color: Constants.textGreyColor,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 21,
+                ),
+              ),
+
+              const Spacer(),
+
+              //Login row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //Login button
+                  Expanded(
+                    child: CustomTextButton.gradient(
+                      width: double.infinity,
+                      onPressed: () {},
+                      gradient: Constants.buttonGradient,
+                      child: const Center(
+                        child: const Text(
+                          "LOGIN",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            letterSpacing: 0.7,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(width: 15),
+
+                  //face id
+                  CustomTextButton.gradient(
+                    width: 60,
+                    onPressed: () {},
+                    gradient: Constants.buttonGradient,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset(AssetsHelper.faceId),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 17),
+
+              //Register button
+              CustomTextButton.outlined(
+                width: double.infinity,
+                onPressed: () {},
+                border: Border.all(color: theme.primaryColor, width: 4),
+                child: Center(
+                  child: Text(
+                    "REGISTER",
+                    style: TextStyle(
+                      color: theme.primaryColor,
+                      fontSize: 15,
+                      letterSpacing: 0.7,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/views/widgets/custom_text_button.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class CustomTextButton extends StatelessWidget {
+  final double height;
+  final double? width;
+  final VoidCallback onPressed;
+  final Color? color;
+  final Border? border;
+  final Gradient? gradient;
+  final double borderRadius;
+  final Widget child;
+
+  const CustomTextButton({
+    Key? key,
+    double? height,
+    double? borderRadius,
+    this.width,
+    this.gradient,
+    this.border,
+    this.color,
+    required this.child,
+    required this.onPressed,
+  })   : borderRadius = borderRadius ?? 7,
+        height = height ?? 60,
+        super(key: key);
+
+  const factory CustomTextButton.gradient({
+    Key? key,
+    double? height,
+    double? width,
+    double? borderRadius,
+    required Widget child,
+    required VoidCallback onPressed,
+    required Gradient gradient,
+  }) = _CustomTextButtonWithGradient;
+
+  const factory CustomTextButton.outlined({
+    Key? key,
+    double? height,
+    double? width,
+    double? borderRadius,
+    required Border border,
+    required Widget child,
+    required VoidCallback onPressed,
+  }) = _CustomTextButtonOutlined;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textButtonTheme = theme.textButtonTheme;
+    return TextButton(
+      style: textButtonTheme.style!.copyWith(
+        overlayColor: MaterialStateProperty.all(theme.primaryColor),
+      ),
+      onPressed: onPressed,
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(borderRadius),
+          border: border,
+          gradient: gradient,
+          color: color,
+        ),
+        child: child,
+      ),
+    );
+  }
+}
+
+class _CustomTextButtonWithGradient extends CustomTextButton {
+  const _CustomTextButtonWithGradient({
+    Key? key,
+    double? height,
+    double? width,
+    double? borderRadius,
+    required Widget child,
+    required VoidCallback onPressed,
+    required Gradient gradient,
+  }) : super(
+          key: key,
+          height: height,
+          width: width,
+          borderRadius: borderRadius,
+          child: child,
+          onPressed: onPressed,
+          gradient: gradient,
+        );
+}
+
+class _CustomTextButtonOutlined extends CustomTextButton {
+  const _CustomTextButtonOutlined({
+    Key? key,
+    double? height,
+    double? width,
+    double? borderRadius,
+    required Border border,
+    required Widget child,
+    required VoidCallback onPressed,
+  }) : super(
+          key: key,
+          height: height,
+          width: width,
+          borderRadius: borderRadius,
+          border: border,
+          child: child,
+          onPressed: onPressed,
+        );
+}
