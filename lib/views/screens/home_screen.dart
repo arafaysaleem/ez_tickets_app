c5ce42a4e4fd6a86395e62d9d490855812b26550
arafaysaleem <a.rafaysaleem@gmail.com>
2021-03-26 21:22:33 +0500

    added login screen and it's route

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
 
 import '../widgets/custom_text_button.dart';
 
 import '../../helper/utils/constants.dart';
 import '../../helper/utils/assets_helper.dart';
+
+import '../../routes/app_router.gr.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -11,104 +14,102 @@ class HomeScreen extends StatelessWidget {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.fromLTRB(20, 125, 20, 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //Heading text
-              Text(
-                "EZ Tickets",
-                style: textTheme.headline1!.copyWith(
-                  color: theme.primaryColor,
-                ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(20, 125, 20, 80),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Heading text
+            Text(
+              "EZ Tickets",
+              style: textTheme.headline1!.copyWith(
+                color: theme.primaryColor,
               ),
+            ),
 
-              const SizedBox(height: 35),
+            const SizedBox(height: 35),
 
-              //Welcome msg
-              Text(
-                "Welcome to\nthe new\nNueplex cinemas",
-                style: textTheme.headline3!.copyWith(
-                  color: Colors.white,
-                ),
+            //Welcome msg
+            Text(
+              "Welcome to\nthe new\nNueplex cinemas",
+              style: textTheme.headline3!,
+            ),
+
+            const SizedBox(height: 40),
+
+            //experience msg
+            Text(
+              "New level of features\nwith the new app",
+              style: textTheme.headline5!.copyWith(
+                color: Constants.textGreyColor,
+                fontWeight: FontWeight.w400,
+                fontSize: 21,
               ),
+            ),
 
-              const SizedBox(height: 40),
+            const Spacer(),
 
-              //experience msg
-              Text(
-                "New level of features\nwith the new app",
-                style: textTheme.headline5!.copyWith(
-                  color: Constants.textGreyColor,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 21,
-                ),
-              ),
-
-              const Spacer(),
-
-              //Login row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //Login button
-                  Expanded(
-                    child: CustomTextButton.gradient(
-                      width: double.infinity,
-                      onPressed: () {},
-                      gradient: Constants.buttonGradient,
-                      child: const Center(
-                        child: const Text(
-                          "LOGIN",
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            letterSpacing: 0.7,
-                            fontWeight: FontWeight.w600,
-                          ),
+            //Login row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //Login button
+                Expanded(
+                  child: CustomTextButton.gradient(
+                    width: double.infinity,
+                    onPressed: () {
+                      AutoRouter.of(context).push(LoginScreenRoute());
+                    },
+                    gradient: Constants.buttonGradientRed,
+                    child: const Center(
+                      child: const Text(
+                        "LOGIN",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          letterSpacing: 0.7,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                   ),
+                ),
 
-                  const SizedBox(width: 15),
+                const SizedBox(width: 15),
 
-                  //face id
-                  CustomTextButton.gradient(
-                    width: 60,
-                    onPressed: () {},
-                    gradient: Constants.buttonGradient,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Image.asset(AssetsHelper.faceId),
-                    ),
+                //face id
+                CustomTextButton.gradient(
+                  width: 60,
+                  onPressed: () {},
+                  gradient: Constants.buttonGradientRed,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(AssetsHelper.faceId),
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
 
-              const SizedBox(height: 17),
+            const SizedBox(height: 17),
 
-              //Register button
-              CustomTextButton.outlined(
-                width: double.infinity,
-                onPressed: () {},
-                border: Border.all(color: theme.primaryColor, width: 4),
-                child: Center(
-                  child: Text(
-                    "REGISTER",
-                    style: TextStyle(
-                      color: theme.primaryColor,
-                      fontSize: 15,
-                      letterSpacing: 0.7,
-                      fontWeight: FontWeight.w600,
-                    ),
+            //Register button
+            CustomTextButton.outlined(
+              width: double.infinity,
+              onPressed: () {},
+              border: Border.all(color: theme.primaryColor, width: 4),
+              child: Center(
+                child: Text(
+                  "REGISTER",
+                  style: TextStyle(
+                    color: theme.primaryColor,
+                    fontSize: 15,
+                    letterSpacing: 0.7,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -1,9 +1,81 @@
 import 'package:flutter/material.dart';
 
+import '../../helper/utils/constants.dart';
+
+import '../widgets/scrollable_column.dart';
+import '../widgets/custom_text_button.dart';
+import '../widgets/rounded_bottom_container.dart';
+import '../widgets/custom_textfield.dart';
+
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: ScrollableColumn(
+          children: [
+            //Input card
+            RoundedBottomContainer(
+              children: [
+                //Page name
+                Text(
+                  "Login",
+                  style: textTheme.headline3!.copyWith(
+                    color: Colors.white,
+                    fontSize: 32,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                //Email
+                const CustomTextField(
+                  floatingText: "Email",
+                  hintText: "Type your email address",
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                ),
+
+                const SizedBox(height: 25),
+
+                //Password
+                const CustomTextField(
+                  floatingText: "Password",
+                  hintText: "Type your password",
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.done,
+                ),
+              ],
+            ),
+
+            const Spacer(),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 40, 20, 80),
+              child: CustomTextButton.gradient(
+                width: double.infinity,
+                onPressed: () {},
+                gradient: Constants.buttonGradientOrange,
+                child: const Center(
+                  child: const Text(
+                    "LOGIN",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      letterSpacing: 0.7,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
