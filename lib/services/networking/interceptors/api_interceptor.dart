93ccb29b755adc4986f4c6d46eb953a501dfed6f
arafaysaleem <a.rafaysaleem@gmail.com>
2021-04-09 13:54:56 -0700

    created a api exception for all exceptions of API
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/services/networking/interceptors/api_interceptor.dart
+++ lib/services/networking/interceptors/api_interceptor.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
--- lib/services/networking/network_exception.dart
+++ lib/services/networking/network_exception.dart
@@ -17,95 +17,20 @@ abstract class NetworkException with _$NetworkException {
     required String message,
   }) = FetchDataException;
 
-  const factory NetworkException.InvalidPropertiesException({
+  const factory NetworkException.ApiException({
     required String name,
     required String message,
-  }) = InvalidPropertiesException;
+  }) = ApiException;
 
-  const factory NetworkException.NotFoundException({
+  const factory NetworkException.TokenExpiredException({
     required String name,
     required String message,
-  }) = NotFoundException;
+  }) = TokenExpiredException;
 
-  const factory NetworkException.DuplicateEntryException({
+  const factory NetworkException.UnrecognizedException({
     required String name,
     required String message,
-  }) = DuplicateEntryException;
-
-  const factory NetworkException.TokenMissingException({
-    required String name,
-    required String message,
-  }) = TokenMissingException;
-
-  const factory NetworkException.InternalServerException({
-    required String name,
-    required String message,
-  }) = InternalServerException;
-
-  const factory NetworkException.UnauthorizedException({
-    required String name,
-    required String message,
-  }) = UnauthorizedException;
-
-  const factory NetworkException.UnexpectedException({
-    required String name,
-    required String message,
-  }) = UnexpectedException;
-
-  const factory NetworkException.CreateFailedException({
-    required String name,
-    required String message,
-  }) = CreateFailedException;
-
-  const factory NetworkException.UpdateFailedException({
-    required String name,
-    required String message,
-  }) = UpdateFailedException;
-
-  const factory NetworkException.InvalidCredentialsException({
-    required String name,
-    required String message,
-  }) = InvalidCredentialsException;
-
-  const factory NetworkException.RegistrationFailedException({
-    required String name,
-    required String message,
-  }) = RegistrationFailedException;
-
-  const factory NetworkException.InvalidEndpointException({
-    required String name,
-    required String message,
-  }) = InvalidEndpointException;
-
-  const factory NetworkException.TokenVerificationException({
-    required String name,
-    required String message,
-  }) = TokenVerificationException;
-
-  const factory NetworkException.OTPGenerationException({
-    required String name,
-    required String message,
-  }) = OTPGenerationException;
-
-  const factory NetworkException.OTPExpiredException({
-    required String name,
-    required String message,
-  }) = OTPExpiredException;
-
-  const factory NetworkException.OTPVerificationException({
-    required String name,
-    required String message,
-  }) = OTPVerificationException;
-
-  const factory NetworkException.ForeignKeyViolationException({
-    required String name,
-    required String message,
-  }) = ForeignKeyViolationException;
-
-  const factory NetworkException.UnimplementedException({
-    required String name,
-    required String message,
-  }) = UnimplementedException;
+  }) = UnrecognizedException;
 
   const factory NetworkException.CancelException({
     required String name,
@@ -156,98 +81,13 @@ abstract class NetworkException with _$NetworkException {
             final name = error.response?.data["headers"]["error"];
             final message = error.response?.data["headers"]["message"];
             switch (name) {
-              case "InvalidPropertiesException":
-                return NetworkException.InvalidPropertiesException(
-                  name: name,
-                  message: message,
-                );
-              case "NotFoundException":
-                return NetworkException.NotFoundException(
-                  name: name,
-                  message: message,
-                );
-              case "DuplicateEntryException":
-                return NetworkException.DuplicateEntryException(
-                  name: name,
-                  message: message,
-                );
-              case "TokenMissingException":
-                return NetworkException.TokenMissingException(
-                  name: name,
-                  message: message,
-                );
-              case "InternalServerException":
-                return NetworkException.InternalServerException(
-                  name: name,
-                  message: message,
-                );
-              case "UnauthorizedException":
-                return NetworkException.UnauthorizedException(
-                  name: name,
-                  message: message,
-                );
-              case "UnexpectedException":
-                return NetworkException.UnexpectedException(
-                  name: name,
-                  message: message,
-                );
-              case "CreateFailedException":
-                return NetworkException.CreateFailedException(
-                  name: name,
-                  message: message,
-                );
-              case "UpdateFailedException":
-                return NetworkException.UpdateFailedException(
-                  name: name,
-                  message: message,
-                );
-              case "InvalidCredentialsException":
-                return NetworkException.InvalidCredentialsException(
-                  name: name,
-                  message: message,
-                );
-              case "RegistrationFailedException":
-                return NetworkException.RegistrationFailedException(
-                  name: name,
-                  message: message,
-                );
-              case "InvalidEndpointException":
-                return NetworkException.InvalidEndpointException(
-                  name: name,
-                  message: message,
-                );
-              case "TokenVerificationException":
-                return NetworkException.TokenVerificationException(
-                  name: name,
-                  message: message,
-                );
-              case "OTPGenerationException":
-                return NetworkException.OTPGenerationException(
-                  name: name,
-                  message: message,
-                );
-              case "OTPExpiredException":
-                return NetworkException.OTPExpiredException(
-                  name: name,
-                  message: message,
-                );
-              case "OTPVerificationException":
-                return NetworkException.OTPVerificationException(
-                  name: name,
-                  message: message,
-                );
-              case "ForeignKeyViolationException":
-                return NetworkException.ForeignKeyViolationException(
-                  name: name,
-                  message: message,
-                );
-              case "UnimplementedException":
-                return NetworkException.UnimplementedException(
+              case "TokenExpiredException":
+                return NetworkException.TokenExpiredException(
                   name: name,
                   message: message,
                 );
               default:
-                return NetworkException.UnexpectedException(
+                return NetworkException.ApiException(
                   name: name,
                   message: message,
                 );
@@ -259,8 +99,8 @@ abstract class NetworkException with _$NetworkException {
           message: "No internet connectivity",
         );
       } else {
-        return NetworkException.UnexpectedException(
-          name: "UnexpectedException",
+        return NetworkException.UnrecognizedException(
+          name: "UnrecognizedException",
           message: "Error unrecognized",
         );
       }
@@ -270,8 +110,8 @@ abstract class NetworkException with _$NetworkException {
         message: e.message,
       );
     } catch (_) {
-      return NetworkException.UnexpectedException(
-        name: "UnexpectedException",
+      return NetworkException.UnrecognizedException(
+        name: "UnrecognizedException",
         message: "Error unrecognized",
       );
     }
