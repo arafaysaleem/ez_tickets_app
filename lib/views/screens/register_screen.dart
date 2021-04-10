7f8cfa11d523bf3993410e4a3da70e039abbaa41
arafaysaleem <a.rafaysaleem@gmail.com>
2021-04-10 07:48:30 -0700

    added auth response to register screen
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/views/screens/register_screen.dart
+++ lib/views/screens/register_screen.dart
@@ -1,5 +1,7 @@
-import 'package:auto_route/auto_route.dart';
+import 'package:ez_ticketz_app/states/auth_state.dart';
+import 'package:ez_ticketz_app/views/widgets/common/welcome_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../helper/extensions/string_extension.dart';
@@ -10,9 +12,6 @@ import '../../helper/utils/constants.dart';
 //Providers
 import '../../providers/all_providers.dart';
 
-//Routes
-import '../../routes/app_router.gr.dart';
-
 //Widgets
 import '../widgets/common/custom_alert_dialog.dart';
 import '../widgets/common/custom_text_button.dart';
@@ -20,7 +19,7 @@ import '../widgets/common/custom_textfield.dart';
 import '../widgets/common/rounded_bottom_container.dart';
 import '../widgets/common/scrollable_column.dart';
 
-class RegisterScreen extends StatefulWidget {
+class RegisterScreen extends StatefulHookWidget {
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
 }
@@ -129,148 +128,179 @@ class _RegisterScreenState extends State<RegisterScreen> {
     ];
   }
 
+  Future<bool> showConfirmDialog(BuildContext context) async {
+    if (_formHasData) {
+      final doPop = await showDialog<bool>(
+        context: context,
+        barrierColor: Constants.barrierColor,
+        builder: (ctx) => const CustomAlertDialog(),
+      );
+      if (doPop == null || !doPop)
+        return Future<bool>.value(false);
+    }
+    return Future<bool>.value(true);
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
+    final authStatus = useProvider(authProvider);
     return Scaffold(
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: ScrollableColumn(
-          children: [
-            //Input card
-            Form(
-              key: formKey,
-              onChanged: () {
-                _formHasData = true;
-              },
-              onWillPop: () async {
-                if (_formHasData) {
-                  final doPop = await showDialog<bool>(
-                    context: context,
-                    barrierColor: Constants.barrierColor,
-                    builder: (ctx) => const CustomAlertDialog(),
-                  );
-                  if (doPop == null || !doPop) return Future<bool>.value(false);
-                }
-                return Future<bool>.value(true);
-              },
-              child: RoundedBottomContainer(
-                onBackTap: !isState1
-                    ? () {
-                        setState(() {
-                          isState1 = true;
-                        });
-                      }
-                    : null,
-                children: [
-                  //Page name
-                  Text(
-                    "Register",
-                    style: textTheme.headline3!.copyWith(
-                      color: Colors.white,
-                      fontSize: 32,
+      body: authStatus.maybeWhen(
+        authenticated: (fullName) {
+          emailController.clear();
+          passwordController.clear();
+          fullNameController.clear();
+          addressController.clear();
+          cPasswordController.clear();
+          contactController.clear();
+          _formHasData = false;
+          return WelcomeWidget(fullName: fullName);
+        },
+        orElse: () => GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: ScrollableColumn(
+            children: [
+              //Input card
+              Form(
+                key: formKey,
+                onChanged: () {
+                  _formHasData = true;
+                },
+                onWillPop: () => showConfirmDialog(context),
+                child: RoundedBottomContainer(
+                  onBackTap: !isState1
+                      ? () {
+                          setState(() {
+                            isState1 = true;
+                          });
+                        }
+                      : null,
+                  children: [
+                    //Page name
+                    Text(
+                      "Register",
+                      style: textTheme.headline3!.copyWith(
+                        color: Colors.white,
+                        fontSize: 32,
+                      ),
                     ),
-                  ),
 
-                  const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-                  if (isState1) ...getState1Fields() else ...getState2Fields(),
-                ],
-              ),
-            ),
+                    if (isState1)
+                      ...getState1Fields()
+                    else
+                      ...getState2Fields(),
 
-            const Spacer(),
-
-            Padding(
-              padding: EdgeInsets.fromLTRB(
-                20,
-                40,
-                20,
-                Constants.bottomInsets,
-              ),
-              child: AnimatedSwitcher(
-                duration: Constants.defaultAnimationDuration,
-                switchOutCurve: Curves.easeInBack,
-                child: isState1
-
-                    //Next Button
-                    ? CustomTextButton.outlined(
-                        width: double.infinity,
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            setState(() {
-                              isState1 = false;
-                            });
-                          }
-                        },
-                        padding: const EdgeInsets.only(left: 20, right: 15),
-                        border: Border.all(color: theme.primaryColor, width: 4),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Next",
-                              style: TextStyle(
-                                color: theme.primaryColor,
-                                fontSize: 15,
-                                letterSpacing: 0.7,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-
-                            //Arrow
-                            Icon(
-                              Icons.arrow_forward_sharp,
-                              size: 26,
-                              color: theme.primaryColor,
-                            )
-                          ],
-                        ),
-                      )
-
-                    //Confirm button
-                    : CustomTextButton.gradient(
-                        width: double.infinity,
-                        onPressed: () async {
-                          if (formKey.currentState!.validate()) {
-                            formKey.currentState!.save();
-                            final registered =
-                                await context.read(authProvider).register(
-                                      email: emailController.text,
-                                      password: passwordController.text,
-                                      fullName: fullNameController.text,
-                                      address: addressController.text,
-                                      contact: contactController.text,
-                                    );
-                            if (registered) {
-                              emailController.clear();
-                              passwordController.clear();
-                              fullNameController.clear();
-                              addressController.clear();
-                              contactController.clear();
-                              cPasswordController.clear();
-                              _formHasData = false;
-                              context.router.push(const MoviesScreenRoute());
-                            }
-                          }
-                        },
-                        gradient: Constants.buttonGradientOrange,
-                        child: const Center(
+                    //Failure reason
+                    if (authStatus is FAILED)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Center(
                           child: Text(
-                            "CONFIRM",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              letterSpacing: 0.7,
-                              fontWeight: FontWeight.w600,
+                            authStatus.reason,
+                            style: textTheme.bodyText1!.copyWith(
+                              fontSize: 18,
+                              color: Constants.primaryColor,
                             ),
                           ),
                         ),
                       ),
+                  ],
+                ),
               ),
-            )
-          ],
+
+              const Spacer(),
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                  20,
+                  40,
+                  20,
+                  Constants.bottomInsets,
+                ),
+                child: AnimatedSwitcher(
+                  duration: Constants.defaultAnimationDuration,
+                  switchOutCurve: Curves.easeInBack,
+                  child: isState1
+
+                      //Next Button
+                      ? CustomTextButton.outlined(
+                          width: double.infinity,
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              setState(() {
+                                isState1 = false;
+                              });
+                            }
+                          },
+                          padding: const EdgeInsets.only(left: 20, right: 15),
+                          border: Border.all(
+                            color: theme.primaryColor,
+                            width: 4,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Next",
+                                style: TextStyle(
+                                  color: theme.primaryColor,
+                                  fontSize: 15,
+                                  letterSpacing: 0.7,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+
+                              //Arrow
+                              Icon(
+                                Icons.arrow_forward_sharp,
+                                size: 26,
+                                color: theme.primaryColor,
+                              )
+                            ],
+                          ),
+                        )
+
+                      //Confirm button
+                      : CustomTextButton.gradient(
+                          width: double.infinity,
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              formKey.currentState!.save();
+                              context.read(authProvider.notifier).register(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                    fullName: fullNameController.text,
+                                    address: addressController.text,
+                                    contact: contactController.text,
+                                  );
+                            }
+                          },
+                          gradient: Constants.buttonGradientOrange,
+                          child: authStatus.maybeWhen(
+                            authenticating: () => const Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                            orElse: () => const Center(
+                              child: Text(
+                                "CONFIRM",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  letterSpacing: 0.7,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
