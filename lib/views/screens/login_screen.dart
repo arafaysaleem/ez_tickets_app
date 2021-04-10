50b27bb741104c5b4a356d86d78b54358901a3a3
arafaysaleem <a.rafaysaleem@gmail.com>
2021-04-10 07:29:02 -0700

    login screen auth state responses added
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,12 +9,13 @@ import '../../helper/utils/constants.dart';
 //Providers
 import '../../providers/all_providers.dart';
 
-//Routes
-import '../../routes/app_router.gr.dart';
+//States
+import '../../states/auth_state.dart';
 
 //Widgets
 import '../widgets/common/custom_text_button.dart';
 import '../widgets/common/custom_textfield.dart';
+import '../widgets/common/welcome_widget.dart';
 import '../widgets/common/rounded_bottom_container.dart';
 import '../widgets/common/scrollable_column.dart';
 
@@ -26,102 +26,125 @@ class LoginScreen extends HookWidget {
   Widget build(BuildContext context) {
     final emailController = useTextEditingController(text: "");
     final passwordController = useTextEditingController(text: "");
+    final authStatus = useProvider(authProvider);
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
     return Scaffold(
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: ScrollableColumn(
-          children: [
-            //Input card
-            Form(
-              key: formKey,
-              child: RoundedBottomContainer(
-                children: [
-                  //Page name
-                  Text(
-                    "Login",
-                    style: textTheme.headline3!.copyWith(
-                      color: Colors.white,
-                      fontSize: 32,
+      body: authStatus.maybeWhen(
+        authenticated: (fullName) {
+          emailController.clear();
+          passwordController.clear();
+          return WelcomeWidget(fullName: fullName);
+        },
+        orElse: () => GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: ScrollableColumn(
+            children: [
+              //Input card
+              Form(
+                key: formKey,
+                child: RoundedBottomContainer(
+                  children: [
+                    //Page name
+                    Text(
+                      "Login",
+                      style: textTheme.headline3!.copyWith(
+                        color: Colors.white,
+                        fontSize: 32,
+                      ),
                     ),
-                  ),
 
-                  const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-                  //Email
-                  CustomTextField(
-                    controller: emailController,
-                    floatingText: "Email",
-                    hintText: "Type your email address",
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.next,
-                    validator: (email) {
-                      if (email != null && email.isValidEmail) return null;
-                      return "Please enter a valid email address";
-                    },
-                  ),
+                    //Email
+                    CustomTextField(
+                      controller: emailController,
+                      floatingText: "Email",
+                      hintText: "Type your email address",
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      validator: (email) {
+                        if (email != null && email.isValidEmail) return null;
+                        return "Please enter a valid email address";
+                      },
+                    ),
 
-                  const SizedBox(height: 25),
+                    const SizedBox(height: 25),
 
-                  //Password
-                  CustomTextField(
-                    controller: passwordController,
-                    floatingText: "Password",
-                    hintText: "Type your password",
-                    keyboardType: TextInputType.visiblePassword,
-                    textInputAction: TextInputAction.done,
-                    validator: (password) {
-                      if (password!.isEmpty) return "Please enter a password";
-                      return null;
-                    },
-                  ),
-                ],
+                    //Password
+                    CustomTextField(
+                      controller: passwordController,
+                      floatingText: "Password",
+                      hintText: "Type your password",
+                      keyboardType: TextInputType.visiblePassword,
+                      textInputAction: TextInputAction.done,
+                      validator: (password) {
+                        if (password!.isEmpty) return "Please enter a password";
+                        return null;
+                      },
+                    ),
+
+                    //Failure reason
+                    if(authStatus is FAILED) Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Center(
+                        child: Text(
+                          authStatus.reason,
+                          style: textTheme.bodyText1!.copyWith(
+                            fontSize: 18,
+                            color: Constants.primaryColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
 
-            const Spacer(),
+              const Spacer(),
 
-            Padding(
-              padding: EdgeInsets.fromLTRB(
-                20,
-                40,
-                20,
-                Constants.bottomInsets,
-              ),
-              child: CustomTextButton.gradient(
-                width: double.infinity,
-                onPressed: () async {
-                  print("ON_PRESSED");
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                    final loggedIn = await context.read(authProvider).login(
-                          email: emailController.text,
-                          password: passwordController.text,
-                        );
-                    if (loggedIn) {
-                      emailController.clear();
-                      passwordController.clear();
-                      context.router.push(const MoviesScreenRoute());
+              //Login button
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                  20,
+                  40,
+                  20,
+                  Constants.bottomInsets,
+                ),
+                child: CustomTextButton.gradient(
+                  width: double.infinity,
+                  onPressed: () async {
+                    print("ON_PRESSED");
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                      context.read(authProvider.notifier).login(
+                        email: emailController.text,
+                        password: passwordController.text,
+                      );
                     }
-                  }
-                },
-                gradient: Constants.buttonGradientOrange,
-                child: const Center(
-                  child: Text(
-                    "LOGIN",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      letterSpacing: 0.7,
-                      fontWeight: FontWeight.w600,
+                  },
+                  gradient: Constants.buttonGradientOrange,
+                  child: authStatus.maybeWhen(
+                    authenticating: () => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    orElse: () => const Center(
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          letterSpacing: 0.7,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
-        ),
+              )
+            ],
+          ),
+        )
       ),
     );
   }
