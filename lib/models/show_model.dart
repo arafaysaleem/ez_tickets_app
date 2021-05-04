002311628ec517aff58b67ba1c6b9da0345abebd
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-04 07:38:03 -0700

    refact(Model): Add an initial factory to show model
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/models/show_model.dart
+++ lib/models/show_model.dart
@@ -6,11 +6,16 @@ import '../enums/show_type_enum.dart';
 part 'show_model.freezed.dart';
 part 'show_model.g.dart';
 
+T? toNull<T>(_) => null;
+
 @freezed
 class ShowModel with _$ShowModel {
+  const ShowModel._();
+
   @JsonSerializable(fieldRename: FieldRename.snake)
   const factory ShowModel({
-    required int showId,
+    @JsonKey(toJson: toNull, includeIfNull: false)
+    @Default(0) int showId,
     required DateTime date,
     required DateTime startTime,
     required DateTime endTime,
@@ -19,6 +24,19 @@ class ShowModel with _$ShowModel {
     required int movieId,
     required int theaterId,
   }) = _ShowModel;
+
+  factory ShowModel.initial(){
+    final dummyDate = DateTime.now();
+    return ShowModel(
+      date: dummyDate,
+      startTime: dummyDate,
+      endTime: dummyDate,
+      movieId: 0,
+      theaterId: 0,
+      showType: ShowType.i2D,
+      showStatus: ShowStatus.FREE
+    );
+  }
 
   factory ShowModel.fromJson(Map<String, dynamic> json) =>
       _$ShowModelFromJson(json);
