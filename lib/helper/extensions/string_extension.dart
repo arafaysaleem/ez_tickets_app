75aa1cbf8b5e515b9380478fc997c71491bb5c63
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-03 19:17:50 -0700

    refact(Helper): Add capitalize case string helper
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/helper/extensions/string_extension.dart
+++ lib/helper/extensions/string_extension.dart
@@ -4,4 +4,5 @@ extension StringExt on String {
   bool get isValidEmail => Constants.emailRegex.hasMatch(this);
   bool get isValidFullName => Constants.fullNameRegex.hasMatch(this);
   bool get isValidContact => Constants.contactRegex.hasMatch(this);
+  String get capitalize => this[0].toUpperCase() + this.substring(1).toLowerCase();
 }
