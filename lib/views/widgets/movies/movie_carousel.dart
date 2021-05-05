9332381f6311a12ae0fc64bf5bf04b8e67a65d7f
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-05 07:55:43 -0700

    refact(Widgets): Fix lint errors in movie carousel widget
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/views/widgets/movies/movie_carousel.dart
+++ lib/views/widgets/movies/movie_carousel.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
