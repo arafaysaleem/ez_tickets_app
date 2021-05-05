7a7d26657b7f0b2b79b641069f1d72bb9be9f004
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-05 09:24:42 -0700

    refact(Screens): extract poster widgets into hooks
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/views/screens/movie_details_screen.dart
+++ lib/views/screens/movie_details_screen.dart
@@ -1,9 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 //Helper
 import '../../helper/utils/constants.dart';
+
+//Providers
+import '../../providers/movies_provider.dart';
 
 //Routes
 import '../../routes/app_router.gr.dart';
@@ -16,56 +20,14 @@ import '../widgets/common/custom_network_image.dart';
 import '../widgets/common/custom_text_button.dart';
 import '../widgets/movie_details/movie_details_sheet.dart';
 
-final Map<String, dynamic> movie = {
-  "title": "The Hustle",
-  "genres": ["Action", "Drama", "Comedy"],
-  "rating": 5.4,
-  "summary":
-      "Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him. Isolated, bullied and disregarded by society, Fleck begins a slow descent into madness as he transforms into the criminal mastermind known as the Joker.",
-  "poster_url":
-      "https://m.media-amazon.com/images/M/MV5BMTc3MDcyNzE5N15BMl5BanBnXkFtZTgwNzE2MDE0NzM@._V1_.jpg",
-  "roles": [
-    {
-      "role_id": 1,
-      "full_name": "Todd Phillips",
-      "age": 29,
-      "picture_url":
-          "https://i.pinimg.com/236x/35/b6/22/35b6221b30d6b7d9cc2d1be2112783d2.jpg",
-      "role_type": "director"
-    },
-    {
-      "role_id": 2,
-      "full_name": "Joaquin Pheonix",
-      "age": 29,
-      "picture_url":
-          "https://resizing.flixster.com/_2uaAm0Gi1CSRRYDH0WNs5puAd0=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/69768/69768_v9_bc.jpg",
-      "role_type": "cast"
-    },
-    {
-      "role_id": 3,
-      "full_name": "Robert De Niro",
-      "age": 29,
-      "picture_url":
-          "https://cps-static.rovicorp.com/2/Open/Getty/Robert%20De%20Niro/_derived_jpg_q90_310x470_m0/186243299.jpg",
-      "role_type": "cast"
-    },
-    {
-      "role_id": 4,
-      "full_name": "Zazie Beets",
-      "age": 29,
-      "picture_url":
-          "https://resizing.flixster.com/t3iiAgmwNQpbDsiVNvNc8s3Zuug=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/981946/981946_v9_bb.jpg",
-      "role_type": "cast"
-    },
-  ],
-};
-
 final leftMoviePoster =
     "https://m.media-amazon.com/images/M/MV5BMTc1NjIzODAxMF5BMl5BanBnXkFtZTgwMTgzNzk1NzM@._V1_.jpg";
 final rightMoviePoster =
     "https://talenthouse-res.cloudinary.com/image/upload/c_limit,f_auto,fl_progressive,h_1280,w_1280/v1568795702/user-1024773/profile/jox3adylqftz1rzurgzz.jpg";
 
 class MovieDetailsScreen extends HookWidget {
+  const MovieDetailsScreen();
+
   @override
   Widget build(BuildContext context) {
     final topGap = 230.0;
@@ -75,32 +37,10 @@ class MovieDetailsScreen extends HookWidget {
         alignment: Alignment.bottomCenter,
         children: [
           //left movie poster
-          Positioned(
-            top: 130,
-            left: 0,
-            height: 250,
-            width: 150,
-            child: CustomNetworkImage(
-              imageUrl: leftMoviePoster,
-              fit: BoxFit.fill,
-              placeholder: const MoviePosterPlaceholder(),
-              errorWidget: const MoviePosterPlaceholder(),
-            ),
-          ),
+          const _LeftMoviePoster(),
 
           //right movie poster
-          Positioned(
-            top: 130,
-            right: 0,
-            height: 250,
-            width: 150,
-            child: CustomNetworkImage(
-              imageUrl: rightMoviePoster,
-              fit: BoxFit.fill,
-              placeholder: const MoviePosterPlaceholder(),
-              errorWidget: const MoviePosterPlaceholder(),
-            ),
-          ),
+          const _RightMoviePoster(),
 
           //Top black overlay
           Positioned(
@@ -114,19 +54,7 @@ class MovieDetailsScreen extends HookWidget {
           ),
 
           //main movie poster
-          Positioned(
-            top: 80,
-            height: 250,
-            width: 190,
-            child: CustomNetworkImage(
-              imageUrl: movie["poster_url"],
-              borderRadius: 10,
-              placeholder: const MoviePosterPlaceholder(),
-              errorWidget: const MoviePosterPlaceholder(),
-              fit: BoxFit.fill,
-              margin: const EdgeInsets.symmetric(horizontal: 10),
-            ),
-          ),
+          const _MainMoviePoster(),
 
           //White details sheet
           Positioned(
@@ -199,6 +127,80 @@ class MovieDetailsScreen extends HookWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class _LeftMoviePoster extends HookWidget {
+  const _LeftMoviePoster({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: Get through provider
+    // final leftMoviePic = useProvider(selectedMovieProvider).state;
+    return Positioned(
+      top: 130,
+      left: 0,
+      height: 250,
+      width: 150,
+      child: CustomNetworkImage(
+        imageUrl: leftMoviePoster,
+        fit: BoxFit.fill,
+        placeholder: const MoviePosterPlaceholder(),
+        errorWidget: const MoviePosterPlaceholder(),
+      ),
+    );
+  }
+}
+
+class _RightMoviePoster extends HookWidget {
+  const _RightMoviePoster({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: Get through provider
+    // final rightMoviePic = useProvider(selectedMovieProvider).state;
+    return Positioned(
+      top: 130,
+      right: 0,
+      height: 250,
+      width: 150,
+      child: CustomNetworkImage(
+        imageUrl: rightMoviePoster,
+        fit: BoxFit.fill,
+        placeholder: const MoviePosterPlaceholder(),
+        errorWidget: const MoviePosterPlaceholder(),
+      ),
+    );
+  }
+}
+
+class _MainMoviePoster extends HookWidget {
+  const _MainMoviePoster({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final posterUrl = useProvider(selectedMovieProvider.select((value) {
+      return value.state.posterUrl;
+    }));
+    return Positioned(
+      top: 80,
+      height: 250,
+      width: 190,
+      child: CustomNetworkImage(
+        imageUrl: posterUrl,
+        borderRadius: 10,
+        placeholder: const MoviePosterPlaceholder(),
+        errorWidget: const MoviePosterPlaceholder(),
+        fit: BoxFit.fill,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
       ),
     );
   }
