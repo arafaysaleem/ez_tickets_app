9b305c1a1832a1ec19921e9844995b289fa4f1d0
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-23 16:54:04 -0700

    docs(Helper): Add docs for constants class
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/helper/utils/constants.dart
+++ lib/helper/utils/constants.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/// A utility class that holds constants for useful and commonly
+/// used values throughout the entire app.
+/// This class has no constructor and all variables are `static`.
 @immutable
 class Constants {
   const Constants._();
 
+  /// The main orange-red color used for theming the app.
   static const Color primaryColor = Color(0xFFf03400);
 
+  /// The color value for red color in the app.
   static const Color redColor = Color(0xFFed0000);
 
+  /// The color value for orange color in the app.
   static const Color orangeColor = Color(0xFFf04f00);
 
+  /// The color value for rating stars in the app.
   static const Color starsColor = Color(0xFFf78040);
 
+  /// The red [LinearGradient] for buttons in the app.
   static const Gradient buttonGradientRed = LinearGradient(
     colors: [primaryColor, redColor],
   );
 
+  /// The orange [LinearGradient] for buttons in the app.
   static const Gradient buttonGradientOrange = LinearGradient(
     colors: [orangeColor, redColor],
   );
 
+  /// The white [LinearGradient] for fading movies carousel in the app.
   static const Gradient movieCarouselGradient = LinearGradient(
     begin: Alignment.bottomCenter,
     end: Alignment.topCenter,
@@ -32,6 +42,7 @@ class Constants {
     ],
   );
 
+  /// The black [LinearGradient] used to overlay movie backgrounds in the app.
   static const Gradient blackOverlayGradient = LinearGradient(
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
@@ -44,37 +55,58 @@ class Constants {
     ],
   );
 
+  /// The color value for dark grey buttons in the app.
   static const Color buttonGreyColor = Color(0xFF1c1c1c);
 
+  /// The color value for dark grey scaffold in the app.
   static const Color scaffoldColor = Color(0xFF141414);
 
+  /// The color value for light grey scaffold in the app.
   static const Color scaffoldGreyColor = Color(0xFF2b2b2b);
 
+  /// The color value for light grey text in the app.
   static const Color textGreyColor = Color(0xFF949494);
 
+  /// The color value for white text in the app.
   static const Color textWhite80Color = Color(0xFFf2f2f2);
 
+  /// The color value for dark grey [CustomDialog] in the app.
   static const Color barrierColor = Colors.black87;
 
+  /// The TextStyle for Lato font in the app.
   static TextStyle latoFont = GoogleFonts.lato().copyWith(color: Colors.black);
 
+  /// The TextStyle for Poppins font in the app.
   static TextStyle poppinsFont = GoogleFonts.poppins().copyWith(
     color: textWhite80Color,
   );
 
+  /// The TextStyle for Roboto font in the app.
   static TextStyle robotoFont = GoogleFonts.roboto();
 
-  static const Duration defaultAnimationDuration = Duration(milliseconds: 550);
+  /// The default [Duration] value for animations in the app.
+  static const Duration defaultAnimationDuration = Duration(milliseconds: 300);
 
+  /// The value for bottom padding of buttons in the app.
+  /// It is measured from the bottom of the screen, that is
+  /// behind the android system navigation.
+  /// Used to prevent overlapping of android navigation with the button.
   static const double bottomInsets = 65;
 
+  /// The value for a smaller bottom padding of buttons in the app.
+  /// It is measured from the bottom of the screen, that is
+  /// behind the android system navigation.
+  /// Used to prevent overlapping of android navigation with the button.
   static const double bottomInsetsLow = 45;
 
+  /// The regular expression for validating emails in the app.
   static RegExp emailRegex = RegExp(
     r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z\.]+\.(com|pk)+",
   );
 
+  /// The regular expression for validating contacts in the app.
   static RegExp contactRegex = RegExp(r"^(03|3)\d{9}$");
 
+  /// The regular expression for validating full names in the app.
   static RegExp fullNameRegex = RegExp(r"^[a-zA-Z ]+$");
 }
