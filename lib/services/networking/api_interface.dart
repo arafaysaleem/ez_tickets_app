974005d4152f55d2aa71b6fb1df498b1f2b5b085
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-22 18:35:23 -0700

    docs(Services): Add documentation to API interface.
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/services/networking/api_interface.dart
+++ lib/services/networking/api_interface.dart
@@ -1,7 +1,26 @@
 import 'package:dio/dio.dart';
 
+/// A base class containing methods for basic API functionality.
+///
+/// Should be implemented by any service class that wishes to interact
+/// with an API.
 abstract class ApiInterface {
+  /// Abstract const constructor. This constructor enables subclasses
+  /// to provide const constructors so that they can be used in
+  /// const expressions.
+  const ApiInterface();
 
+  /// Base method for requesting collection of data from the [endpoint].
+  /// The response is **deserialized** into a List of model objects of type [T],
+  /// using the [builder] callback.
+  ///
+  /// [queryParams] holds any query parameters for the request.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [requiresAuthToken] is used to decide if a token will be inserted
+  /// in the **headers** of the request using an [ApiInterceptor].
   Future<List<T>> getCollectionData<T>({
     required String endpoint,
     Map<String, dynamic>? queryParams,
@@ -10,6 +29,17 @@ abstract class ApiInterface {
     required T Function(Map<String, dynamic> responseBody) builder,
   });
 
+  /// Base method for requesting a document of data from the [endpoint].
+  /// The response is deserialized into a single model objects of type [T],
+  /// using the [builder] callback.
+  ///
+  /// [queryParams] holds any query parameters for the request.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [requiresAuthToken] is used to decide if a token will be inserted
+  /// in the **headers** of the request using an [ApiInterceptor]
   Future<T> getDocumentData<T>({
     required String endpoint,
     Map<String, dynamic>? queryParams,
@@ -18,6 +48,15 @@ abstract class ApiInterface {
     required T Function(Map<String, dynamic> responseBody) builder,
   });
 
+  /// Base method for inserting [data] at the [endpoint].
+  /// The response is deserialized into an object of type [T],
+  /// using the [builder] callback.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [requiresAuthToken] is used to decide if a token will be inserted
+  /// in the **headers** of the request using an [ApiInterceptor]
   Future<T> setData<T>({
     required String endpoint,
     required Map<String, dynamic> data,
@@ -26,6 +65,15 @@ abstract class ApiInterface {
     required T Function(Map<String, dynamic> response) builder,
   });
 
+  /// Base method for updating [data] at the [endpoint].
+  /// The response is deserialized into an object of type [T],
+  /// using the [builder] callback.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [requiresAuthToken] is used to decide if a token will be inserted
+  /// in the **headers** of the request using an [ApiInterceptor]
   Future<T> updateData<T>({
     required String endpoint,
     required Map<String, dynamic> data,
@@ -34,6 +82,15 @@ abstract class ApiInterface {
     required T Function(Map<String, dynamic> response) builder,
   });
 
+  /// Base method for deleting [data] at the [endpoint].
+  /// The response is deserialized into an object of type [T],
+  /// using the [builder] callback.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [requiresAuthToken] is used to decide if a token will be inserted
+  /// in the **headers** of the request using an [ApiInterceptor]
   Future<T> deleteData<T>({
     required String endpoint,
     Map<String, dynamic>? data,
@@ -42,5 +99,9 @@ abstract class ApiInterface {
     required T Function(Map<String, dynamic> response) builder,
   });
 
+  /// Base method for cancelling requests pre-maturely
+  /// using the [cancelToken].
+  ///
+  /// If null, the **default** [cancelToken] inside [DioService] is used.
   void cancelRequests({CancelToken? cancelToken});
 }
