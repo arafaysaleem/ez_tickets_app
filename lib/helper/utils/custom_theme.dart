ce13a4de1dc8c9cccb3259c632a5465d4d2adaf0
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-23 16:54:21 -0700

    docs(Helper): Add docs for custom themes class
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/helper/utils/custom_theme.dart
+++ lib/helper/utils/custom_theme.dart
@@ -2,63 +2,71 @@ import 'package:flutter/material.dart';
 
 import 'constants.dart';
 
+/// A utility class that holds themes for the app.
+/// This class has no constructor and all methods are `static`.
 @immutable
 class CustomTheme {
   const CustomTheme._();
 
-  static ThemeData get mainTheme {
-    return ThemeData(
-      primaryColor: Constants.primaryColor,
-      scaffoldBackgroundColor: Constants.scaffoldColor,
-      fontFamily: Constants.poppinsFont.fontFamily,
-      textTheme: TextTheme(
-        headline1: Constants.poppinsFont.copyWith(
-          fontWeight: FontWeight.bold,
-          fontSize: 50,
-          height: 1.15,
-        ),
-        headline2: Constants.latoFont.copyWith(
-          fontWeight: FontWeight.bold,
-          fontSize: 50,
-          height: 1.15,
-        ),
-        headline3: Constants.poppinsFont.copyWith(
-          fontWeight: FontWeight.w400,
-          fontSize: 26,
-          height: 1.15,
-        ),
-        headline4: Constants.latoFont.copyWith(
-          fontWeight: FontWeight.w500,
-          fontSize: 26,
-          height: 1.15,
-        ),
-        headline5: Constants.poppinsFont.copyWith(
-          fontWeight: FontWeight.w300,
-          fontSize: 20,
-          height: 1.15,
-        ),
-        headline6: Constants.latoFont.copyWith(
-          fontWeight: FontWeight.w300,
-          fontSize: 20,
-          height: 1.15,
-        ),
-        subtitle1: Constants.poppinsFont.copyWith(
-          fontWeight: FontWeight.w200,
-        ),
-        subtitle2: Constants.latoFont.copyWith(
-          fontWeight: FontWeight.w200,
-        ),
-        bodyText1: Constants.poppinsFont.copyWith(
-          fontWeight: FontWeight.w400,
-        ),
-        bodyText2: Constants.latoFont.copyWith(
-          fontWeight: FontWeight.w400,
-        ),
+  /// The main starting theme for the app.
+  ///
+  /// Sets the following defaults:
+  /// * primaryColor: [Constants.primaryColor],
+  /// * scaffoldBackgroundColor: [Constants.scaffoldColor],
+  /// * fontFamily: [Constants.poppinsFont].fontFamily,
+  /// * iconTheme: [Colors.white] for default icon
+  /// * textButtonTheme: [TextButtonTheme] without the default padding,
+  static late final mainTheme = ThemeData(
+    primaryColor: Constants.primaryColor,
+    scaffoldBackgroundColor: Constants.scaffoldColor,
+    fontFamily: Constants.poppinsFont.fontFamily,
+    textTheme: TextTheme(
+      headline1: Constants.poppinsFont.copyWith(
+        fontWeight: FontWeight.bold,
+        fontSize: 50,
+        height: 1.15,
       ),
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(padding: const EdgeInsets.all(0)),
+      headline2: Constants.latoFont.copyWith(
+        fontWeight: FontWeight.bold,
+        fontSize: 50,
+        height: 1.15,
       ),
-      iconTheme: const IconThemeData(color: Colors.white),
-    );
-  }
+      headline3: Constants.poppinsFont.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 26,
+        height: 1.15,
+      ),
+      headline4: Constants.latoFont.copyWith(
+        fontWeight: FontWeight.w500,
+        fontSize: 26,
+        height: 1.15,
+      ),
+      headline5: Constants.poppinsFont.copyWith(
+        fontWeight: FontWeight.w300,
+        fontSize: 20,
+        height: 1.15,
+      ),
+      headline6: Constants.latoFont.copyWith(
+        fontWeight: FontWeight.w300,
+        fontSize: 20,
+        height: 1.15,
+      ),
+      subtitle1: Constants.poppinsFont.copyWith(
+        fontWeight: FontWeight.w200,
+      ),
+      subtitle2: Constants.latoFont.copyWith(
+        fontWeight: FontWeight.w200,
+      ),
+      bodyText1: Constants.poppinsFont.copyWith(
+        fontWeight: FontWeight.w400,
+      ),
+      bodyText2: Constants.latoFont.copyWith(
+        fontWeight: FontWeight.w400,
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(padding: const EdgeInsets.all(0)),
+    ),
+    iconTheme: const IconThemeData(color: Colors.white),
+  );
 }
