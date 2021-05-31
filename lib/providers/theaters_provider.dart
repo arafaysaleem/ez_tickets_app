4b3661d19a1c65d14973cbea2d490a42d4294d17
arafaysaleem <a.rafaysaleem@gmail.com>
2021-05-31 02:24:44 -0700

    refact(Provider): Add update/insert/remove methods in theater provider
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/providers/theaters_provider.dart
+++ lib/providers/theaters_provider.dart
@@ -1,22 +1,24 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-//Services
-import '../services/repositories/theaters_repository.dart';
-
 //Enums
 import '../enums/theater_type_enum.dart';
 
 //Models
 import '../models/theater_model.dart';
+import '../models/seat_model.dart';
+
+//Services
+import '../services/repositories/theaters_repository.dart';
 
 //Providers
 import 'all_providers.dart';
 
 final showTheaterFuture = FutureProvider.family.autoDispose<TheaterModel, int>(
-      (ref, theaterId) async {
+  (ref, theaterId) async {
     final _theatersProvider = ref.watch(theatersProvider);
 
-    final theater = await _theatersProvider.getTheaterById(theaterId: theaterId);
+    final theater =
+        await _theatersProvider.getTheaterById(theaterId: theaterId);
 
     return theater;
   },
@@ -42,70 +44,54 @@ class TheatersProvider {
     return await _theatersRepository.fetchOne(theaterId: theaterId);
   }
 
-  // Future<TheaterModel> uploadNewTheater({
-  //   required int movieId,
-  //   required int theaterId,
-  //   required DateTime startTime,
-  //   required DateTime endTime,
-  //   required DateTime date,
-  //   required TheaterType theaterType,
-  //   required TheaterStatus theaterStatus,
-  // }) async {
-  //   final data = <String, dynamic>{
-  //     "movie_id": movieId,
-  //     "theater_id": theaterId,
-  //     "start_time": startTime,
-  //     "end_time": endTime,
-  //     "date": date,
-  //     "theater_type": theaterType.toJson,
-  //     "theater_status": theaterStatus.toJson,
-  //   };
-  //   final theaterId = await _theatersRepository.create(data: data);
-  //   final theaterTime = TheaterTimeModel(
-  //     theaterId: theaterId,
-  //     startTime: startTime,
-  //     endTime: endTime,
-  //     theaterStatus: theaterStatus,
-  //     theaterType: theaterType,
-  //     theaterId: theaterId,
-  //   );
-  //
-  //   final theater = TheaterModel(
-  //       date: date,
-  //       movieId: movieId,
-  //       theaterTimes: [theaterTime]
-  //   );
-  //   return theater;
-  // }
-  //
-  // Future<String> editTheater({
-  //   required int theaterId,
-  //   int? movieId,
-  //   int? theaterId,
-  //   DateTime? startTime,
-  //   DateTime? endTime,
-  //   DateTime? date,
-  //   TheaterType? theaterType,
-  //   TheaterStatus? theaterStatus,
-  // }) async {
-  //   final data = <String, dynamic>{
-  //     if (movieId != null) "movie_id": movieId,
-  //     if (theaterId != null) "theater_id": theaterId,
-  //     if (startTime != null) "start_time": startTime,
-  //     if (endTime != null) "end_time": endTime,
-  //     if (date != null) "date": date,
-  //     if (theaterType != null) "theater_type": theaterType.toJson,
-  //     if (theaterStatus != null) "theater_status": theaterStatus.toJson,
-  //   };
-  //   if (data.isEmpty) return "Nothing to update!";
-  //   return await _theatersRepository.update(theaterId: theaterId, data: data);
-  // }
-  //
-  // Future<String> removeTheater({
-  //   required int theaterId,
-  // }) async {
-  //   return await _theatersRepository.delete(theaterId: theaterId);
-  // }
+  Future<TheaterModel> uploadNewTheater({
+    required String theaterName,
+    required int numOfRows,
+    required int seatsPerRow,
+    required TheaterType theaterType,
+    required List<SeatModel> missing,
+    required List<SeatModel> blocked,
+  }) async {
+    final theater = TheaterModel(
+      theaterId: null,
+      theaterName: theaterName,
+      numOfRows: numOfRows,
+      seatsPerRow: seatsPerRow,
+      theaterType: theaterType,
+      missing: missing,
+      blocked: blocked,
+    );
+    final theaterId = await _theatersRepository.create(data: theater.toJson());
+
+    return theater.copyWith(theaterId: theaterId);
+  }
+
+  Future<String> editTheater({
+    required int theaterId,
+    String? theaterName,
+    int? numOfRows,
+    int? seatsPerRow,
+    TheaterType? theaterType,
+    List<SeatModel>? missing,
+    List<SeatModel>? blocked,
+  }) async {
+    final data = <String,dynamic>{
+      if(theaterName != null) "theater_name": theaterName,
+      if(numOfRows != null) "num_of_rows": numOfRows,
+      if(seatsPerRow != null) "seats_per_row": seatsPerRow,
+      if(theaterType != null) "theater_type": theaterType.toJson,
+      if(missing != null) "missing": missing.map((s) => s.toJson()).toList(),
+      if(blocked != null) "blocked": blocked.map((s) => s.toJson()).toList(),
+    };
+    if (data.isEmpty) return "Nothing to update!";
+    return await _theatersRepository.update(theaterId: theaterId, data: data);
+  }
+
+  Future<String> removeTheater({
+    required int theaterId,
+  }) async {
+    return await _theatersRepository.delete(theaterId: theaterId);
+  }
 
   void cancelNetworkRequest() {
     _theatersRepository.cancelRequests();
