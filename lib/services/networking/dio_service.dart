fa70b1435a1f57959935a90e4ba676290bb00189
arafaysaleem <a.rafaysaleem@gmail.com>
2021-06-01 14:04:40 -0700

    docs(Services): Add docs for the Dio service class and methods
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/services/networking/dio_service.dart
+++ lib/services/networking/dio_service.dart
@@ -10,14 +10,33 @@ import 'interceptors/refresh_token_interceptor.dart';
 //Exceptions
 import 'network_exception.dart';
 
+/// A service class that wraps the [Dio] instance and provides methods for
+/// basic network requests.
 class DioService {
+
+  /// An instance of [Dio] for executing network requests.
   late final Dio _dio;
+
+  /// An instance of [CancelToken] used to pre-maturely cancel
+  /// network requests.
   late final CancelToken _cancelToken;
 
+  /// A public constructor that is used to create the Dio service
+  /// with [baseOptions].
+  ///
+  /// Calls [createDio()] to setup the underlying [_dio] instance.
   DioService({required BaseOptions baseOptions}) {
     createDio(baseOptions);
   }
 
+  /// An method to create new instance of [Dio] with [baseOptions].
+  ///
+  /// Attaches any external [Interceptors] to [_dio].
+  ///
+  /// * [ApiInterceptor] handles token injection and response success validation
+  /// * [LoggingInterceptor] performs logging of all network requests, only
+  /// in debug mode
+  /// * [RefreshTokenInterceptor] refreshes an expired token.
   void createDio(BaseOptions baseOptions) {
     _cancelToken = CancelToken();
     _dio = Dio(baseOptions);
@@ -28,6 +47,9 @@ class DioService {
     ]);
   }
 
+  /// This method invokes the [cancel()] method on either the input
+  /// [cancelToken] or internal [_cancelToken] to pre-maturely end all
+  /// requests attached to this token.
   void cancelRequests({CancelToken? cancelToken}) {
     if (cancelToken == null) {
       _cancelToken.cancel('Cancelled');
@@ -36,6 +58,18 @@ class DioService {
     }
   }
 
+  /// This method sends a `GET` request to the [endpoint] and returns the
+  /// **decoded** response.
+  ///
+  /// Any errors encountered during the request are caught and a custom
+  /// [NetworkException] is thrown.
+  ///
+  /// [queryParams] holds any query parameters for the request.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [options] are special instructions that can be merged with the request.
   Future<Map<String, dynamic>> get({
     required String endpoint,
     Map<String, dynamic>? queryParams,
@@ -55,6 +89,18 @@ class DioService {
     }
   }
 
+  /// This method sends a `POST` request to the [endpoint] and returns the
+  /// **decoded** response.
+  ///
+  /// Any errors encountered during the request are caught and a custom
+  /// [NetworkException] is thrown.
+  ///
+  /// The [data] contains body for the request.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [options] are special instructions that can be merged with the request.
   Future<Map<String, dynamic>> post({
     required String endpoint,
     Map<String, dynamic>? data,
@@ -74,6 +120,18 @@ class DioService {
     }
   }
 
+  /// This method sends a `PATCH` request to the [endpoint] and returns the
+  /// **decoded** response.
+  ///
+  /// Any errors encountered during the request are caught and a custom
+  /// [NetworkException] is thrown.
+  ///
+  /// The [data] contains body for the request.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [options] are special instructions that can be merged with the request.
   Future<Map<String, dynamic>> patch({
     required String endpoint,
     Map<String, dynamic>? data,
@@ -93,6 +151,18 @@ class DioService {
     }
   }
 
+  /// This method sends a `DELETE` request to the [endpoint] and returns the
+  /// **decoded** response.
+  ///
+  /// Any errors encountered during the request are caught and a custom
+  /// [NetworkException] is thrown.
+  ///
+  /// The [data] contains body for the request.
+  ///
+  /// [cancelToken] is used to cancel the request pre-maturely. If null,
+  /// the **default** [cancelToken] inside [DioService] is used.
+  ///
+  /// [options] are special instructions that can be merged with the request.
   Future<Map<String, dynamic>> delete({
     required String endpoint,
     Map<String, dynamic>? data,
