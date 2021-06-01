2ea79048b99b85937e60e665cfb1ec519f80b63a
arafaysaleem <a.rafaysaleem@gmail.com>
2021-06-01 11:40:26 -0700

    feat(Screen): Display movie actors skeleton loader during loading
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/views/widgets/movie_details/movie_actors_list.dart
+++ lib/views/widgets/movie_details/movie_actors_list.dart
@@ -24,6 +24,9 @@ import '../../skeletons/actor_picture_placeholder.dart';
 //Widgets
 import '../common/custom_network_image.dart';
 
+//Skeletons
+import '../../skeletons/movie_actors_skeleton_loader.dart';
+
 final movieRolesFuture = FutureProvider.family<List<MovieRoleModel>, int>(
   (ref, movieId) async {
     final _moviesProvider = ref.watch(moviesProvider);
@@ -55,64 +58,64 @@ class MovieActorsList extends HookWidget {
       return value.state.movieId;
     }));
     final movieRoles = useProvider(movieRolesFuture(movieId!));
-    return movieRoles.when(
-      data: (movieRoles) => Column(
-        children: [
-          //Actors title
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Cast And Crew",
-                style: textTheme.headline2!.copyWith(
-                  color: Colors.black,
-                  fontSize: 17,
-                ),
+    return Column(
+      children: [
+        //Actors title
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Cast And Crew",
+              style: textTheme.headline2!.copyWith(
+                color: Colors.black,
+                fontSize: 17,
               ),
             ),
           ),
-
-          const SizedBox(height: 10),
-
-          //Actors list
-          SizedBox(
-            height: 140,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              physics: const BouncingScrollPhysics(),
-              separatorBuilder: (ctx, i) => const SizedBox(width: 15),
-              itemCount: movieRoles.length,
-              itemBuilder: (ctx, i) {
-                final mRole = movieRoles[i].role;
-                final isLast = i == (movieRoles.length - 1);
-                final isFirst = i == 0;
-                return Padding(
-                  padding: getImagePadding(isFirst: isFirst, isLast: isLast),
-                  child: _ActorListItem(
-                    pictureUrl: mRole.pictureUrl,
-                    fullName: mRole.fullName,
-                    roleType: movieRoles[i].roleType.inString,
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-      loading: () => const Center(
-        child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(Constants.buttonGreyColor),
         ),
-      ),
-      error: (error, st) {
-        if (error is NetworkException) {
-          return Text(error.message);
-        }
-        debugPrint(error.toString());
-        debugPrint(st.toString());
-        return Text(error.toString());
-      },
+
+        const SizedBox(height: 10),
+
+        //Actors list
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 550),
+          switchOutCurve: Curves.easeInBack,
+          child: movieRoles.when(
+            data: (movieRoles) => SizedBox(
+              height: 140,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+                separatorBuilder: (ctx, i) => const SizedBox(width: 15),
+                itemCount: movieRoles.length,
+                itemBuilder: (ctx, i) {
+                  final mRole = movieRoles[i].role;
+                  final isLast = i == (movieRoles.length - 1);
+                  final isFirst = i == 0;
+                  return Padding(
+                    padding: getImagePadding(isFirst: isFirst, isLast: isLast),
+                    child: _ActorListItem(
+                      pictureUrl: mRole.pictureUrl,
+                      fullName: mRole.fullName,
+                      roleType: movieRoles[i].roleType.inString,
+                    ),
+                  );
+                },
+              ),
+            ),
+            loading: () => const MovieActorsSkeletonLoader(),
+            error: (error, st) {
+              if (error is NetworkException) {
+                return Text(error.message);
+              }
+              debugPrint(error.toString());
+              debugPrint(st.toString());
+              return Text(error.toString());
+            },
+          ),
+        ),
+      ],
     );
   }
 }
