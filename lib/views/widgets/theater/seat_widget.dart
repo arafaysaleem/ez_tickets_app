ab571c29d91921c5cf3c223ed5b81be83e85fd87
arafaysaleem <a.rafaysaleem@gmail.com>
2021-06-01 01:24:32 -0700

    refact(Widget): Add bounce animation on theater seat select
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/views/widgets/theater/seat_widget.dart
+++ lib/views/widgets/theater/seat_widget.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //Helpers
 import '../../../helper/utils/constants.dart';
 
-//Providers
-import '../../../providers/all_providers.dart';
-
 //Models
 import '../../../models/seat_model.dart';
 
-class SeatWidget extends StatefulWidget {
+//Providers
+import '../../../providers/all_providers.dart';
+
+class SeatWidget extends StatefulHookWidget {
   final SeatModel seat;
 
   const SeatWidget({
@@ -25,35 +26,49 @@ class SeatWidget extends StatefulWidget {
 class _SeatWidgetState extends State<SeatWidget> {
   bool isSelected = false;
 
+  void _onTap(){
+    setState(() {
+      isSelected = !isSelected;
+    });
+    final _theatersProvider = context.read(theatersProvider);
+    if (isSelected) {
+      _theatersProvider.selectSeat(widget.seat);
+    } else {
+      _theatersProvider.unSelectSeat(widget.seat);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final animController = useAnimationController(
+      duration: const Duration(milliseconds: 90),
+    );
+    final bounceAnimation = Tween<double>(begin: 1.0, end: 0.5).animate(
+      CurvedAnimation(
+        parent: animController,
+        curve: Curves.easeInQuad,
+      ),
+    );
+    useEffect((){
+      bounceAnimation.addStatusListener((status) {
+        if(status == AnimationStatus.completed) animController.reverse();
+      });
+    }, const []);
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          isSelected = !isSelected;
-          final _theatersProvider = context.read(theatersProvider);
-          if(isSelected){
-            _theatersProvider.selectSeat(widget.seat);
-          }
-          else {
-            _theatersProvider.unSelectSeat(widget.seat);
-          }
-        });
-      },
-      child: isSelected
-          ? const DecoratedBox(
-        decoration: BoxDecoration(
-          color: Constants.redColor,
-          borderRadius: BorderRadius.all(
-            Radius.circular(8),
-          ),
+      onTap: _onTap,
+      onTapDown: (_) => animController.forward(),
+      child: AnimatedBuilder(
+        animation: bounceAnimation,
+        builder: (ctx, child) => Transform.scale(
+          scale: bounceAnimation.value,
+          child: child,
         ),
-      )
-          : const DecoratedBox(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(
-            Radius.circular(8),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: isSelected ? Constants.redColor : Colors.white,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(8),
+            ),
           ),
         ),
       ),
