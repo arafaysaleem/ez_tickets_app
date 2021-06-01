85ff94c85de2556944cf3825a0d31ddf767019f2
arafaysaleem <a.rafaysaleem@gmail.com>
2021-06-01 13:07:09 -0700

    docs(Extensions): Add docs for all extensions
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/helper/extensions/int_extension.dart
+++ lib/helper/extensions/int_extension.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: unnecessary_this
 
+/// A utility with extensions for integers.
 extension IntExt on int {
+  /// An extension converting int to Duration in milliseconds.
   Duration get milliseconds => Duration(milliseconds: this);
+
+  /// An extension converting int to Duration in microseconds.
   Duration get microseconds => Duration(microseconds: this);
+
+  /// An extension converting int to Duration in seconds.
   Duration get seconds => Duration(seconds: this);
 }
--- lib/helper/extensions/string_extension.dart
+++ lib/helper/extensions/string_extension.dart
@@ -1,11 +1,20 @@
 // ignore_for_file: unnecessary_this
 import '../utils/constants.dart';
 
+/// A utility with extensions for strings.
 extension StringExt on String {
+  /// An extension for validating String as an email.
   bool get isValidEmail => Constants.emailRegex.hasMatch(this);
+
+  /// An extension for validating String as a full name.
   bool get isValidFullName => Constants.fullNameRegex.hasMatch(this);
+
+  /// An extension for validating String as an contact.
   bool get isValidContact => Constants.contactRegex.hasMatch(this);
-  String get capitalize =>
-      this[0].toUpperCase() + this.substring(1).toLowerCase();
+
+  /// An extension for converting String to Capitalcase.
+  String get capitalize => this[0].toUpperCase() + this.substring(1).toLowerCase();
+
+  /// An extension for replacing underscores in a String with spaces.
   String get removeUnderScore => this.replaceAll("_", " ");
 }
