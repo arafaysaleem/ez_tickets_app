40cc11f75b3ef95bfe04bfac6af7ee6eb6ac758e
arafaysaleem <a.rafaysaleem@gmail.com>
2021-06-01 06:40:53 -0700

    feat(Skeleton): Add movies screen skeleton widget
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

---
+++ lib/views/skeletons/movies_skeleton_loader.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+//Helpers
+import '../../helper/utils/constants.dart';
+
+//Widgets
+import '../widgets/common/shimmer_loader.dart';
+
+class MoviesSkeletonLoader extends StatelessWidget {
+  const MoviesSkeletonLoader();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return ShimmerLoader(
+      child: Container(
+        color: Constants.scaffoldGreyColor,
+        height: screenHeight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            //Movie icon
+            const Expanded(
+              child: Icon(
+                Icons.movie_creation_rounded,
+                color: Constants.darkSkeletonColor,
+                size: 65,
+              ),
+            ),
+
+            //Movie Carousel Skeleton
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                //Left container
+                Container(
+                  height: 0.62 * screenHeight,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                    color: Constants.lightSkeletonColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(0, 20, 20, 200),
+                  child: const DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Constants.darkSkeletonColor,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+
+                const Spacer(),
+
+                //Center container
+                Container(
+                  height: 0.68 * screenHeight,
+                  width: 235,
+                  decoration: const BoxDecoration(
+                    color: Constants.lightSkeletonColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30),
+                    ),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(20,20,20,Constants.bottomInsetsLow),
+                  child: Column(
+                    children: const [
+                      //Image Box
+                      SizedBox(
+                        width: double.infinity,
+                        height: 255,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Constants.darkSkeletonColor,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                        ),
+                      ),
+
+                      Spacer(),
+
+                      //Title Box
+                      SizedBox(
+                        height: 45,
+                        width: 160,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Constants.darkSkeletonColor,
+                            borderRadius: BorderRadius.all(Radius.circular(7)),
+                          ),
+                        ),
+                      ),
+
+                      Spacer(),
+
+                      //Chips Box
+                      SizedBox(
+                        height: 30,
+                        width: double.infinity,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Constants.darkSkeletonColor,
+                            borderRadius: BorderRadius.all(Radius.circular(7)),
+                          ),
+                        ),
+                      ),
+
+                      Spacer(),
+
+                      //Ratings Box
+                      SizedBox(
+                        height: 20,
+                        width: 140,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Constants.darkSkeletonColor,
+                            borderRadius: BorderRadius.all(Radius.circular(7)),
+                          ),
+                        ),
+                      ),
+
+                      Spacer(),
+
+                      //Button Box
+                      SizedBox(
+                        height: 55,
+                        width: double.infinity,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Constants.darkSkeletonColor,
+                            borderRadius: BorderRadius.all(Radius.circular(7)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const Spacer(),
+
+                //Right Container
+                Container(
+                  height: 0.62 * screenHeight,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                    color: Constants.lightSkeletonColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                    ),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(20, 20, 0, 200),
+                  child: const DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Constants.darkSkeletonColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
