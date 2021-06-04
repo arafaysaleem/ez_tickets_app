1297ed9fbdfd3e3060f425fd8733da270c0d825d
arafaysaleem <a.rafaysaleem@gmail.com>
2021-06-04 08:53:09 -0700

    refact(Widget): Add routing and payment processing to pay button
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- lib/views/widgets/payment/pay_button.dart
+++ lib/views/widgets/payment/pay_button.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:auto_route/auto_route.dart';
 
 //Helpers
 import '../../../helper/utils/constants.dart';
+
+//Routes
+import '../../../routes/app_router.gr.dart';
+
+//Providers
+import '../../../providers/all_providers.dart';
 
 //Widgets
 import '../common/custom_text_button.dart';
@@ -15,7 +23,10 @@ class PayButton extends StatelessWidget {
       padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
       child: CustomTextButton.gradient(
         width: double.infinity,
-        onPressed: () {},
+        onPressed: () {
+          context.read(paymentsProvider).makePayment();
+          context.router.push(const ConfirmationScreenRoute());
+        },
         gradient: Constants.buttonGradientOrange,
         child: const Center(
           child: Text(
