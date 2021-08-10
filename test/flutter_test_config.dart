bdf16530a26d37062f5b284f64717a0ce4efd6e3
arafaysaleem <a.rafaysaleem@gmail.com>
2021-08-10 16:11:32 -0700

    test(Goldens): add a global golden test config
    Signed-off-by: arafaysaleem <a.rafaysaleem@gmail.com>

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:golden_toolkit/golden_toolkit.dart';
 
+//Theme
+import 'package:ez_ticketz_app/helper/utils/custom_theme.dart';
+
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   return GoldenToolkit.runWithConfiguration(
     () async {
@@ -9,11 +12,21 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async {
       await testMain();
     },
     config: GoldenToolkitConfiguration(
-      defaultDevices: const [Device.iphone11],
+      defaultDevices: const [GoldensGlobalConfig.defaultDevice],
       deviceFileNameFactory: (name, device) {
         // return 'goldens/$name.${device.name}.png';
         return 'goldens/$name.png';
       }
     ),
   );
+}
+
+abstract class GoldensGlobalConfig {
+  static final globalAppWrapper = materialAppWrapper(
+    theme: CustomTheme.mainTheme,
+  );
+
+  static const defaultDevice = Device.iphone11;
+
+  static final defaultSurfaceSize = defaultDevice.size;
 }
